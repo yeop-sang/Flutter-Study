01ff1d7b6d67c52efea8a9d04d00263365bdb1e2
yeop <yeop@yeop.kr>
2022-07-13 15:00:52 +0900

    [Start Flutter] 3. Add a Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ void main() {
   runApp(const MyApp());
 }
 
+/* Stateless Widget
+ * Immutable하여 속성들이 변하지 않는다. 즉, 해당 value들은 모두 final 변수이다.
+ * 변하는 widget을 만들고 싶으면 Stateful widget을 이용해야 한다.
+ */
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -17,10 +21,30 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Title Text'),
         ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
+        body: const Center(
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+/*
+ * Stateful widget
+ * Create Stateful widget with stful keyword
+ */
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
